dafacf9a7f531691ff49b5a8fa2ebf6b06610c97
Maksym Bezhevets <begik4@gmail.com>
2024-01-16 15:08:53 +0200

    Updated settings.py for User model

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "theatre",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -103,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
