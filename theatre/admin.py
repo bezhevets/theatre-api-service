1edb41c330adf90dff3545d13325e68ecb33f0ba
Maksym Bezhevets <begik4@gmail.com>
2024-01-16 23:16:41 +0200

    Updated admin panel for TheatreHall

--- theatre/admin.py
+++ theatre/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 from theatre.models import TheatreHall, Genre, Actor
 
 # Register your models here.
-admin.site.register(TheatreHall)
 admin.site.register(Genre)
 admin.site.register(Actor)
+
+
+@admin.register(TheatreHall)
+class TheatreHallAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "rows",
+        "seats_in_row",
+        "capacity",
+    ]
