db87af1eaa6ae0def900f4d4e8cce9df04aeb5dd
Maksym Bezhevets <begik4@gmail.com>
2024-01-17 20:41:27 +0200

    Set DEFAULT_AUTHENTICATION - TokenAuthentication

--- app/settings.py
+++ app/settings.py
@@ -141,5 +141,8 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 15
+    "PAGE_SIZE": 15,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
 }
