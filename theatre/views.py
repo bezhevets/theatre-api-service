ffd312cf114241ad89990885b765125f8cf4254e
Maksym Bezhevets <begik4@gmail.com>
2024-01-17 13:21:12 +0200

    Added filtering to PlayViewSet by title, genres, actors.

--- theatre/views.py
+++ theatre/views.py
@@ -41,8 +41,34 @@ class TheatreHallViewSet(
 class PlayViewSet(
     viewsets.ModelViewSet
 ):
-    queryset = Play.objects.all().prefetch_related("actors", "genres")
+    queryset = Play.objects.prefetch_related("actors", "genres")
     serializer_class = PlaySerializer
+
+    @staticmethod
+    def _params_str_to_int(params_in_url: str) -> list:
+        """Converts a list of string IDs to a list of integers"""
+        return [int(param) for param in params_in_url.split(",")]
+
+    def get_queryset(self):
+        """Retrieve the movies with filters"""
+        title = self.request.query_params.get("title")
+        genres = self.request.query_params.get("genres")
+        actors = self.request.query_params.get("actors")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if genres:
+            genres_id = self._params_str_to_int(genres)
+            queryset = queryset.filter(genres__id__in=genres_id)
+
+        if actors:
+            actors_id = self._params_str_to_int(actors)
+            queryset = queryset.filter(actors__id__in=actors_id)
+
+        return queryset.distinct()
 
     def get_serializer_class(self):
         if self.action == "list":
