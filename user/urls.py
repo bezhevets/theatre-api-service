e55f61f9669313d48a73f6b3e78dca7837ee062d
Maksym Bezhevets <begik4@gmail.com>
2024-01-17 19:43:42 +0200

    Added TokenAuthentication and create users endpoints (register, login, me).

---
+++ user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from user.views import CreateUserView, CreateTokenView, ManageUserView
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
+
+
+app_name = "user"
