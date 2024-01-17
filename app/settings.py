ea2eae691350062429f28f9ac66b8ef86e37cbff
Maksym Bezhevets <begik4@gmail.com>
2024-01-17 13:35:36 +0200

    The pagination style was set globally.

--- app/settings.py
+++ app/settings.py
@@ -135,3 +135,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 15
+}
