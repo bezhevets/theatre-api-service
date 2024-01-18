31c0bbc4c5a745a10f9604690679c5641591071d
Maksym Bezhevets <begik4@gmail.com>
2024-01-18 23:35:34 +0200

    Updated User serializers and views (email login).

--- user/views.py
+++ user/views.py
@@ -4,7 +4,7 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -14,11 +14,11 @@ class CreateUserView(generics.CreateAPIView):
 
 class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = AuthTokenSerializer
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
 
     def get_object(self):
