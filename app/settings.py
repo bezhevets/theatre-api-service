db90c3b2016b457134c1b4ad2afe580382ffdc05
Maksym Bezhevets <begik4@gmail.com>
2024-01-20 17:32:45 +0200

    Added a JWT support for the project.

--- app/settings.py
+++ app/settings.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 15,
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "theatre.permissions.IsAdminOrIfAuthenticatedReadOnly",
